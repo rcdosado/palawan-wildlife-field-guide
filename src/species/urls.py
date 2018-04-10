2037a3ad3911ff5a9214ea2606c6e8a0eb3b0d05
roy <rcdosado@gmail.com>
2018-04-11 07:34:33 +0800

    fix duplicate sql queries, css cleanups, detailview, listview cleanups

--- src/species/urls.py
+++ src/species/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from species.views import SpeciesListView,SpeciesDetailView
 
 urlpatterns = [
-    url(r'^detail/(?P<slug>[-\w]+)$', SpeciesDetailView.as_view(), name='species-detail'),
+    url(r'^detail/(?P<pk>\d+)$', SpeciesDetailView.as_view(), name='species-detail'),
+    # url(r'^detail/(?P<slug>[-\w]+)$', SpeciesDetailView.as_view(), name='species-detail'),
     url(r'^$', SpeciesListView.as_view(), name='species-list'),
 ]
--- src/species/views.py
+++ src/species/views.py
@@ -2,19 +2,20 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from project_msit.settings import base
 
-from species.models import Species 
+from species.models import Species,Category
 
 class SpeciesListView(ListView):
 	model = Species 
-	context_object_name = 'species_list'
-	queryset = Species.objects.all()
+	queryset = Species.objects.select_related('classname__group_as',
+		'kingdom','phylum','order','family','genus','created_by').all().order_by('-modified')		
 	template_name = 'species/list_species.html'
 
 	def get_context_data(self, **kwargs):
 		context = super(SpeciesListView, self).get_context_data(**kwargs)
-		context['now'] = timezone.now()
-		# import pdb; pdb.set_trace()
+		# context['now'] = timezone.now()
+		context['category_list'] = Category.objects.all()
 		return context
 
 class SpeciesDetailView(DetailView):
@@ -23,9 +24,19 @@ class SpeciesDetailView(DetailView):
 	template_name = 'species/detailview.html'
 	# slug_field = 'slug'
 
+	def get_queryset(self):
+		#import pdb; pdb.set_trace()
+		if 'pk' in self.kwargs:
+			return Species.objects.select_related('classname__group_as','kingdom',
+				'phylum','order','family','genus','created_by').filter(id=self.kwargs['pk'])
+		return None		
+
 	def get_context_data(self, **kwargs):
 		context = super().get_context_data(**kwargs)
-		context['now'] = timezone.now()
+		# import pdb; pdb.set_trace()
+		# context['now'] = timezone.now()
+		#import pdb; pdb.set_trace()
+		context['category_list'] = Category.objects.all().order_by('title')
 		return context	
 
 
