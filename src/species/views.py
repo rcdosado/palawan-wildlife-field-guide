4435ab2e080472a5756b8fd62347dab2e3de3c78
roy <rcdosado@gmail.com>
2018-04-11 10:43:05 +0800

    add basic search feature uses Q object

--- src/species/views.py
+++ src/species/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from project_msit.settings import base
+from django.db.models import Q
 
 from species.models import Species,Category
 
@@ -11,6 +12,31 @@ class SpeciesListView(ListView):
 	queryset = Species.objects.select_related('classname__group_as',
 		'kingdom','phylum','order','family','genus','created_by').all().order_by('-modified')		
 	template_name = 'species/list_species.html'
+
+	def get_queryset(self):
+		'''
+			we simply override the queryset, and get the default queryset(that one above), 
+			and filter it according to parameters from users
+			we use Q object for this
+		'''
+		qs = super(SpeciesListView, self).get_queryset()
+		user_query = self.request.GET.get("q")
+		user_filter = self.request.GET.get("search_param")
+		# import pdb; pdb.set_trace()
+		if user_query:
+			user_query=user_query.strip()
+			return qs.filter(
+				Q(kingdom__title__icontains=user_query) |
+				Q(phylum__title__icontains=user_query) |
+				Q(order__title__icontains=user_query) |
+				Q(family__title__icontains=user_query) |
+				Q(genus__title__icontains=user_query) |
+				Q(specie__icontains=user_query) |
+				Q(sciname_author__icontains=user_query) |
+				Q(classname__group_as__title__icontains=user_query) |
+				Q(created_by__name__icontains=user_query) 
+			)
+		return qs 
 
 	def get_context_data(self, **kwargs):
 		context = super(SpeciesListView, self).get_context_data(**kwargs)
